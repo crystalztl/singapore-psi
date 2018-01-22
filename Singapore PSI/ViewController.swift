09869d613ca1717e21520c1d105a3c581fc0c96d
Zhang Tianli <tianli@m-daq.com>
2018-01-22 18:03:12 +0800

    Initial Commit

---
+++ Singapore PSI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Singapore PSI
+//
+//  Created by Zhang Tianli on 22/1/18.
+//  Copyright © 2018 tianli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
